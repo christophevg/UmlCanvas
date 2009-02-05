a8a6703cc5f7f6d2433420e999a54f5c2db6466a
Christophe VG <xtof@pumpkin.local>
2009-02-05 21:27:23 +0100

    added plugin info

--- src/Model.js
+++ src/Model.js
@@ -23,6 +23,13 @@ UmlCanvas.fireEvent = function( event, data ) {
 };
 
 UmlCanvas.Model = Class.create( Canvas2D.Canvas, {
+    initialize: function($super, props) {
+	$super(props);
+	this.registerPlugin
+	( { name: "UmlCanvas",
+	    author: "<a href=\"http://christophe.vg\">Christophe VG</a>",
+	    info: "Visit <a href=\"http://thesoftwarefactory.be/wiki/UmlCanvas\">http://thesoftwarefactory.be/wiki/UmlCanvas</a> for more info." } );
+    },
     addDiagram: function(diagram) {
 	UmlCanvas.diagrams[diagram.name] = diagram;
 	return this.addSheet(diagram);
