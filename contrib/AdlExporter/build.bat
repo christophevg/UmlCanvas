df8e1c3ff1874a3a469c2917f74ee1eebdd935c1
Christophe VG <xtof@pumpkin.local>
2010-02-15 12:03:48 +0100

    removed AdlExporter

--- contrib/AdlExporter/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-PATH=C:\PROGRA~1\MONO-2.2\bin;%PATH%
-echo building Addin...
-call gmcs -target:library -out:addin.dll -r:System.Windows.Forms.dll -r:Interop.EA.dll addin.cs
-echo done
-pause
--- contrib/AdlExporter/reg.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo registering assemby addin.dll
-call %WINDIR%\Microsoft.NET\Framework\v2.0.50727\regasm addin.dll /codebase
-echo registering addin with EA
-call regedit /s addin.reg
-pause
