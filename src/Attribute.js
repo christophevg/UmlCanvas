3bfc4d3778b9fc9a231e98c685de1dfb316fb11d
Christophe VG <xtof@pumpkin.local>
2010-03-05 13:32:07 +0100

    fixes #345: added stereotype modifier to attributes and operations

--- src/Attribute.js
+++ src/Attribute.js
@@ -1,54 +1,73 @@
 UmlCanvas.Attribute = Class.extend( {
-    init: function( attribute ) {
-	this.visibility = attribute.visibility;
-	this.ztatic     = attribute.isStatic;
-	this.name       = attribute.name;
-	this.type       = attribute.type;
-    },
+  init: function( attribute ) {
+    this.visibility = attribute.visibility;
+    this.ztatic     = attribute.isStatic;
+    this.name       = attribute.name;
+    this.type       = attribute.type;
+    this.stereotype = attribute.stereotype;
+  },
 
-    setParent: function setParent() {},
+  setParent: function setParent() {},
 
-    getName:       function() { return this.name;       },
-    getType:       function() { return this.type;       },
-    getVisibility: function() { return this.visibility; },
-    isStatic:      function() { return this.ztatic;     },
+  getName:       function() { return this.name;       },
+  getType:       function() { return this.type;       },
+  getVisibility: function() { return this.visibility; },
+  isStatic:      function() { return this.ztatic;     },
+  getStereotype: function() { return this.stereotype; },
 
-    toString: function() {
-	return UmlCanvas.Common.determineVisibility(this.visibility)
-	    + this.name + (this.type ? " : " + this.type.toString() : "");
-    },
+  toString: function() {
+    return ( this.getStereotype() ? "<<" + this.getStereotype() + ">> " : "" ) 
+      + UmlCanvas.Common.determineVisibility(this.visibility)
+      + this.name + (this.type ? " : " + this.type.toString() : "");
+  },
 
-    asConstruct: function() {
-	var modifiers = {};
-	if( this.getVisibility() ) {
-	    modifiers[this.getVisibility()] = null;
-	};
-	if( this.isStatic() ) { modifiers["static"] = null; }
-	return {
-		 type        : "Attribute",
-		 name        : this.getName(),
-		 supers      : this.getType() ? [ this.getType() ] : [],
-		 modifiers   : modifiers,
-		 children    : []
-	       };
+  asConstruct: function() {
+    var modifiers = {};
+    if( this.getVisibility() ) {
+      modifiers[this.getVisibility()] = null;
+    };
+
+    if( this.isStatic() ) { modifiers["static"] = null; }
+
+    if( this.getStereotype() ) {
+      modifiers["stereotype"] = '"' + this.getStereotype() + '"';
     }
+
+    return {
+      type        : "Attribute",
+      name        : this.getName(),
+      supers      : this.getType() ? [ this.getType() ] : [],
+      modifiers   : modifiers,
+      children    : []
+    };
+  }
 } );
 
 UmlCanvas.Attribute.from = function(construct, clazz) {
-    var props =  { name: construct.name, 
-	type: construct.supers[0] ? construct.supers[0].toString() : null };
-    var visibility = UmlCanvas.Common.extractVisibility(construct);
-    if( visibility ) {
-	props['visibility'] = visibility;
-    }
-    props['isStatic'] = UmlCanvas.Common.extractStatic(construct);
+  var props =  { 
+    name: construct.name, 
+    type: construct.supers[0] ? construct.supers[0].toString() : null 
+  };
 
-    return clazz.addAttribute(props);
+  var visibility = UmlCanvas.Common.extractVisibility(construct);
+  if( visibility ) {
+    props['visibility'] = visibility;
+  }
+
+  props['isStatic'] = UmlCanvas.Common.extractStatic(construct);
+
+  // STEREOTYPE
+  var stereotype = construct.modifiers.get("stereotype" );
+  if( stereotype && stereotype.value ) {
+    props.stereotype = stereotype.value.value;
+  }
+
+  return clazz.addAttribute(props);
 };
 
 UmlCanvas.Attribute.MANIFEST = {
-    name: "attribute",
-    aliasses: [ "property", "literal" ]
+  name: "attribute",
+  aliasses: [ "property", "literal" ]
 }
 
 Canvas2D.registerShape(UmlCanvas.Attribute);
--- src/Operation.js
+++ src/Operation.js
@@ -1,83 +1,97 @@
 UmlCanvas.Operation = Class.extend( {
-    init: function( operation ) {
-	this.visibility = operation.visibility;
-	this.ztatic     = operation.isStatic;
-	this.name       = operation.name;
-	this.returnType = operation.returnType;
-	this.parameters = [];
-	if( operation.arguments ) {
-	    for(var p=0; p<operation.arguments.length; p++ ) {
-		this.addParameter(operation.arguments[p]);
-	    }
-	}
-	this.abztract   = operation.isAbstract;
-    },
-
-    setParent: function setParent() {},
-
-    getName:       function() { return this.name;       },
-    getReturnType: function() { return this.returnType; },
-    getVisibility: function() { return this.visibility; },
-    isStatic:      function() { return this.ztatic;     },
-    isAbstract:    function() { return this.abztract;   },
-
-    addParameter: function(parameter) {
-	this.parameters.push( new UmlCanvas.Parameter(parameter) );
-    },
-
-    toString: function() {
-	var params = [];
-	this.parameters.iterate(function(param) {
-	    if( param.type ) {
-		params.push( param.type.toString() );
-	    }
-	});
-	return UmlCanvas.Common.determineVisibility(this.visibility)
-	    + this.name + "(" + params.join( ", " ) + ")"
-	    + (this.returnType ? " : " + this.returnType.toString() : "");
-    },
-
-    asConstruct: function() {
-	var parameters = [];
-	this.parameters.iterate(function(parameter) {
-	    parameters.push(parameter.asConstruct());
-	});
-	var modifiers = {};
-	if( this.getVisibility() ) {
-	    modifiers[this.getVisibility()] = null;
-	};
-	if( this.isAbstract() ) {
-	    modifiers['abstract'] = null;
-	}
-	if( this.isStatic() ) { modifiers["static"] = null; }
-	return {
-		 type        : "Operation",
-		 name        : this.getName(),
-		 supers      : this.getReturnType() ? [ this.getReturnType() ] : [],
-		 modifiers   : modifiers,
-		 children    : parameters
-	       };
+  init: function( operation ) {
+    this.visibility = operation.visibility;
+    this.ztatic     = operation.isStatic;
+    this.name       = operation.name;
+    this.returnType = operation.returnType;
+    this.stereotype = operation.stereotype;
+    this.parameters = [];
+    if( operation.arguments ) {
+      for(var p=0; p<operation.arguments.length; p++ ) {
+        this.addParameter(operation.arguments[p]);
+      }
     }
+    this.abztract   = operation.isAbstract;
+  },
+
+  setParent: function setParent() {},
+
+  getName:       function() { return this.name;       },
+  getReturnType: function() { return this.returnType; },
+  getVisibility: function() { return this.visibility; },
+  isStatic:      function() { return this.ztatic;     },
+  isAbstract:    function() { return this.abztract;   },
+  getStereotype: function() { return this.stereotype; },
+
+  addParameter: function(parameter) {
+    this.parameters.push( new UmlCanvas.Parameter(parameter) );
+  },
+
+  toString: function() {
+    var params = [];
+    this.parameters.iterate(function(param) {
+      if( param.type ) {
+        params.push( param.type.toString() );
+      }
+    });
+    return ( this.getStereotype() ? "<<" + this.getStereotype() + ">> " : "" ) 
+      + UmlCanvas.Common.determineVisibility(this.visibility)
+      + this.name + "(" + params.join( ", " ) + ")"
+      + (this.returnType ? " : " + this.returnType.toString() : "");
+  },
+
+  asConstruct: function() {
+    var parameters = [];
+    this.parameters.iterate(function(parameter) {
+      parameters.push(parameter.asConstruct());
+    });
+    var modifiers = {};
+    if( this.getVisibility() ) {
+      modifiers[this.getVisibility()] = null;
+    };
+    if( this.isAbstract() ) {
+      modifiers['abstract'] = null;
+    }
+    if( this.isStatic() ) { modifiers["static"] = null; }
+
+    if( this.getStereotype() ) {
+      modifiers["stereotype"] = '"' + this.getStereotype() + '"';
+    }    
+    
+    return {
+      type        : "Operation",
+      name        : this.getName(),
+      supers      : this.getReturnType() ? [ this.getReturnType() ] : [],
+      modifiers   : modifiers,
+      children    : parameters
+    };
+  }
 } );
 
 UmlCanvas.Operation.from = function(construct, clazz) {
-    var props =  { name: construct.name }; 
-    if( construct.supers[0] ) {
-	props.returnType = construct.supers[0].toString();
-    }
-    var visibility = UmlCanvas.Common.extractVisibility(construct);
-    if( visibility ) {
-	props['visibility'] = visibility;
-    }
-    props['isStatic'] = UmlCanvas.Common.extractStatic(construct);
-    props['isAbstract'] = UmlCanvas.Common.extractAbstract(construct);
+  var props =  { name: construct.name }; 
+  if( construct.supers[0] ) {
+    props.returnType = construct.supers[0].toString();
+  }
+  var visibility = UmlCanvas.Common.extractVisibility(construct);
+  if( visibility ) {
+    props['visibility'] = visibility;
+  }
+  props['isStatic'] = UmlCanvas.Common.extractStatic(construct);
+  props['isAbstract'] = UmlCanvas.Common.extractAbstract(construct);
 
-    return clazz.addOperation( props );
+  // STEREOTYPE
+  var stereotype = construct.modifiers.get("stereotype" );
+  if( stereotype && stereotype.value ) {
+    props.stereotype = stereotype.value.value;
+  }
+
+  return clazz.addOperation( props );
 };
 
 UmlCanvas.Operation.MANIFEST = {
-    name     : "operation",
-    aliasses : [ "method" ]
+  name     : "operation",
+  aliasses : [ "method" ]
 }
 
 Canvas2D.registerShape(UmlCanvas.Operation);
